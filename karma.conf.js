57bd9e52233b0b3f9fc5bd382e5e33593efd9634
Florian Knop <florian_knop@hotmail.com>
2016-11-08 17:15:55 +0100

    Cleaning up 5.0.0

--- karma.conf.js
+++ karma.conf.js
@@ -14,47 +14,17 @@ module.exports = function (config) {
         },
 
         webpack: {
-
         },
-        // files: [
-            // 'jspm_packages/npm/zone.js@0.6.18/dist/zone.js',
-            // 'jspm_packages/npm/reflect-metadata@0.1.8/Reflect.js'
-        // ],
-        
-        // proxies: {
-        //     '/index.ts': '/base/index.ts',
-        //     '/math.ts': '/base/math.ts',
-        //     '/aggregate.ts': '/base/aggregate.ts',
-        //     '/boolean.ts': '/base/boolean.ts',
-        //     '/array.ts': '/base/array.ts',
-        //     '/string.ts': '/base/string.ts',
-        //     '/object.ts': '/base/object.ts',
-        //     '/test': '/base/test',
-        //     '/src': '/base/src',
-        //     '/jspm_packages': '/base/jspm_packages',
-        //     '/node_modules': '/base/node_modules',
-        //     '/tsconfig.json': '/base/tsconfig.json'
-        // },
-        
-        // jspm: {
-        //     serveFiles: [
-        //         '*.ts',
-        //         'src/**/*.ts',
-        //         'test/**/*.ts',
-        //         'tsconfig.json'
-        //     ],
-        //     loadFiles: [
-        //         'test/main.spec.ts'
-        //     ],
-        //     packages: 'jspm_packages',
-        //     config: 'jspm.config.js',
-        //     browser: 'jspm.browser.js'
-        // },
-        
+
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+
         frameworks: ['jasmine'],
         browsers: ['Firefox'],
         plugins: [
             'karma-webpack',
+            'karma-coverage',
             'karma-jasmine',
             'karma-spec-reporter',
             'karma-firefox-launcher'

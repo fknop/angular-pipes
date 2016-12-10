3de03085a161e8331978a590f76d528e4e367b01
Florian Knop <florian_knop@hotmail.com>
2016-12-10 15:53:04 +0100

    Add gulp clang format

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,20 @@
 
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const format = require('gulp-clang-format');
+
+const srcsToFmt = ['path/**/*.ts'];
+
+gulp.task('check-format', function() {
+  return gulp.src(srcsToFmt)
+     .pipe(format.checkFormat());
+});
+
+gulp.task('format', function() {
+  return gulp.src(srcsToFmt, { base: '.' })
+      .pipe(format.format())
+      .pipe(gulp.dest('.'));
+});
 
 gulp.task('clean', function () {
    

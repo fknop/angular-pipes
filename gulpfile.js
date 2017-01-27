e247f3a24db9973cd3822de892dc12c93bae2083
Florian Knop <florian_knop@hotmail.com>
2017-01-27 15:30:00 +0100

    Update ignore / files included

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,11 @@ const clean = require('gulp-clean');
 gulp.task('clean', function () {
    
    return gulp.src([
-     'src/**/*.ngfactory.ts',
      'src/**/*.js',
      'src/**/*.d.ts',
      'src/**/*.js.map',
      'src/**/*.metadata.json',
-     'src/**/*.ngsummary.json',
-     'test/**/*.js',
-     'test/**/*.map',
-     'test/**/*.d.ts'
+     'src/**/*.ngsummary.json'
    ]).pipe(clean()); 
 });
 

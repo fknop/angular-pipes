0c8902767be596742ad18c6b011cdf16f8d679fa
Baso10 <baso10@users.noreply.github.com>
2018-04-26 21:11:31 +0200

    feat: implement Math.abs pipe (#64)

---
+++ src/math/abs.pipe.spec.ts
@@ -0,0 +1,27 @@
+import { AbsPipe } from './abs.pipe';
+        
+
+
+describe('AbsPipe', () => {
+    
+    let pipe: AbsPipe;
+    
+    beforeEach(() => {
+       pipe = new AbsPipe(); 
+    });
+    
+    it('Should return 2', () => {
+        
+        expect(pipe.transform(2)).toEqual(2);
+    });
+    
+    it('Should return 2', () => {
+       
+        expect(pipe.transform(-2)).toEqual(2); 
+    });
+
+    it('Should return NaN', () => {
+        expect(pipe.transform('a')).toEqual('NaN');
+    });
+  
+});

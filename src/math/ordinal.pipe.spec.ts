7d99fed9b3ef89ac61362c703bb6eaad7ab9e28c
Tanay Vardhan <tvardha2@illinois.edu>
2018-08-17 14:02:50 +0530

    added test case for st to improve coverage

--- src/math/ordinal.pipe.spec.ts
+++ src/math/ordinal.pipe.spec.ts
@@ -8,17 +8,22 @@ describe('OrdinalPipe', () => {
        pipe = new OrdinalPipe(); 
     });
     
-    it('Should return rd', () => {
+    it('Should return 123rd', () => {
         
         expect(pipe.transform(123)).toEqual('123rd');
     });
+
+    it('Should return 221st', () => {
+        
+        expect(pipe.transform(221)).toEqual('221st');
+    });
     
-    it('Should return nd', () => {
+    it('Should return 2nd', () => {
        
         expect(pipe.transform(2)).toEqual('2nd'); 
     });
 
-    it('Should return th', () => {
+    it('Should return 15th', () => {
        
         expect(pipe.transform(15)).toEqual('15th'); 
     });

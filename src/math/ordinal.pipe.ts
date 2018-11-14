360d454b7a30686df203d10f7d3d68453b8a8956
Christophe El Khoury <christophe.elkhoury@saradarbank.com>
2018-11-14 10:38:50 +0200

    refactor: spacing refactor

--- src/math/ordinal.pipe.ts
+++ src/math/ordinal.pipe.ts
@@ -1,12 +1,12 @@
-import { Pipe, PipeTransform  } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { isNumberFinite } from '../utils/utils';
 
 @Pipe({
-  name: 'ordinal'
+    name: 'ordinal'
 })
 export class OrdinalPipe implements PipeTransform {
-  
-    transform (input: any): any {
+
+    transform(input: any): any {
 
         if (!isNumberFinite(input)) {
             return 'NaN';
@@ -14,16 +14,16 @@ export class OrdinalPipe implements PipeTransform {
 
         const cardinal = input.toString().charAt(input.toString().length - 1);
 
-        switch(cardinal) {
+        switch (cardinal) {
             case '1':
                 return input + 'st';
             case '2':
                 return input + 'nd';
             case '3':
                 return input + 'rd';
-            default: 
+            default:
                 return input + 'th';
         }
-        
+
     }
 }
